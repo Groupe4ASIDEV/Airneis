c21a93f3aac22c7fb908c75f72ff0ad26a4902db
JulienJes <91883647+JulienJes@users.noreply.github.com>
2023-12-07 21:03:32 +0100

    template ongoing

---
+++ front/src/pages/Home.js
@@ -0,0 +1,29 @@
+import Carousel from '../components/Carousel';
+
+function Home() {
+    return (
+        <div>
+            <Carousel />
+            <div>
+                <p>
+                    <strong>
+                        Venant des Hautes Terres d'Ecosse
+                        <br />
+                        Nos meubles sont immortels
+                    </strong>
+                </p>
+            </div>
+            <div>
+                <p>3 highlighted categories</p>
+            </div>
+            <div>
+                <p>
+                    <strong>Les highlanders du moment</strong>
+                </p>
+                <p>highlighted products</p>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
