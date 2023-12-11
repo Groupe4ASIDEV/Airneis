ee8b09c4a7d0f138a9650159df4c6a82d9318dc2
Julien Jesionek <91883647+JulienJes@users.noreply.github.com>
2023-12-11 10:41:07 +0100

    headers implemented with MUI & styles

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,10 @@
+import Header from './components/Header';
 import Index from './routes';
 
 function App() {
     return (
         <>
+            <Header />
             <main>
                 <div className="container">
                     <Index />
---
+++ front/src/components/Header.js
@@ -0,0 +1,56 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Header() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography
+                        variant="h1"
+                        component="h1"
+                        className="title-main"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        Àirneis
+                    </Typography>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <ShoppingCartIcon />
+                    </IconButton>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <SearchIcon />
+                    </IconButton>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
+
+export default Header;
