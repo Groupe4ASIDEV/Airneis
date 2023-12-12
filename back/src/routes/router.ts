b6134a69a2f9ce3d5eac33eef6d3746831c5f68f
Julien Jesionek <91883647+JulienJes@users.noreply.github.com>
2023-12-12 13:35:46 +0100

    userRouter configured

--- back/src/routes/router.ts
+++ back/src/routes/router.ts
@@ -1,7 +1,17 @@
 import Router from 'koa-router';
 import StatusController from '../controllers/StatusController';
+import UserController from '../controllers/UserController';
 
-const router: Router = new Router({ prefix: '/status' });
-router.get('/', StatusController.status);
+const router: Router = new Router();
+router.get('/status', StatusController.status);
+
+const userRouter = new Router({ prefix: '/user' });
+userRouter.get('/', UserController.getAll);
+userRouter.get('/:id', UserController.getOneById);
+userRouter.put('/:id', UserController.update);
+userRouter.delete('/:id', UserController.delete);
+userRouter.post('/create', UserController.create);
+
+router.use(userRouter.routes(), userRouter.allowedMethods());
 
 export default router;
