b02f9f2082829b19bbc9dac5a5f76a645addc8f8
JulienJes <91883647+JulienJes@users.noreply.github.com>
2023-12-13 18:35:36 +0100

    front ongoing

--- front/src/routes/index.js
+++ front/src/routes/index.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
+import SignUpForm from '../components/Log/SignUpForm';
 
 function Index() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path='/signup' exact="true" element={<SignUpForm />} />
         </Routes>
     );
 }
