40b39a6fde1c5f7abf2c77e96cf8f33ec4fa0afc
Laura KURTZ <laura.kurtz057@laposte.net>
2024-01-22 19:20:00 +0100

    front category in progres, needs testing

---
+++ front/src/components/CategoryList.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { Typography, Box } from '@mui/material';
+import axios from 'axios';
+
+const CategoryList = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/category');
+                setCategories(response.data);
+            } catch (error) {
+                console.error('Erreur lors du chargement des catégories :', error);
+            }
+        };
+
+        fetchCategories();
+    }, []);
+
+    return (
+        <div>
+            <Typography variant="h4" component="h2" gutterBottom>
+                Toutes les catégories
+            </Typography>
+            <ul>
+                {categories.map(category => (
+                    <li key={category._id}>
+                        <strong>{category.label}</strong>: {category.description}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default CategoryList;
