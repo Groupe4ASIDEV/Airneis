e13b3549fe5ea5ad083786bfb941fde2ced97ddd
Laura KURTZ <laura.kurtz057@laposte.net>
2024-03-19 19:42:44 +0100

    bug to fix on update autocomplete from existing data OK

---
+++ front/src/components/UserSettings.js
@@ -0,0 +1,105 @@
+import React, {useContext, useState} from 'react';
+import { Typography, TextField, Button } from '@mui/material';
+import {UidContext} from "./Authentication/UserContext";
+
+
+const UserSettings = () => {
+    const { userId } = useContext(UidContext);
+    const [userData, setUserData] = useState({
+        fullName: '',
+        email: '',
+        password: '',
+        phone: '',
+        validated: false,
+        defaultAddress: ''
+    });
+
+    console.log(userData);
+
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setUserData({ ...userData, [name]: value });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch(`/user/update/${userId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userData)
+            });
+
+            if (response.ok) {
+                alert('Paramètres du compte utilisateur mis à jour avec succès !');
+            } else {
+                const errorData = await response.json();
+                alert(`Une erreur s'est produite : ${errorData.message}`);
+            }
+        } catch (error) {
+            console.error("Erreur lors de la mise à jour des paramètres du compte utilisateur :", error);
+            alert("Une erreur s'est produite lors de la mise à jour des paramètres du compte utilisateur.");
+        }
+    };
+
+    return (
+        <div>
+            <Typography variant="h4" gutterBottom>Paramètres du compte utilisateur</Typography>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    label="Nom complet"
+                    variant="outlined"
+                    fullWidth
+                    name="fullName"
+                    value={userData.fullName}
+                    onChange={handleChange}
+                    margin="normal"
+                />
+                <TextField
+                    label="Email"
+                    variant="outlined"
+                    fullWidth
+                    type="email"
+                    name="email"
+                    value={userData.email}
+                    onChange={handleChange}
+                    margin="normal"
+                />
+                <TextField
+                    label="Mot de passe"
+                    variant="outlined"
+                    fullWidth
+                    type="password"
+                    name="password"
+                    value={userData.password}
+                    onChange={handleChange}
+                    margin="normal"
+                />
+                <TextField
+                    label="Téléphone"
+                    variant="outlined"
+                    fullWidth
+                    name="phone"
+                    value={userData.phone}
+                    onChange={handleChange}
+                    margin="normal"
+                />
+                <TextField
+                    label="Adresse par défaut"
+                    variant="outlined"
+                    fullWidth
+                    name="defaultAddress"
+                    value={userData.defaultAddress}
+                    onChange={handleChange}
+                    margin="normal"
+                />
+                <Button type="submit" variant="contained" color="primary">Enregistrer</Button>
+            </form>
+        </div>
+    );
+};
+
+export default UserSettings;
--- front/src/pages/Authentication.js
+++ front/src/pages/Authentication.js
@@ -1,10 +1,14 @@
 import { useContext } from 'react';
 import SignIn from '../components/Authentication/SignInForm';
+import UserSettings from '../components/UserSettings';
 import { UidContext } from '../components/Authentication/UserContext';
 
 function LogIn() {
     const { isAuth } = useContext(UidContext);
-    return <div id="authentication">{isAuth ? <p>USER CONNECTED</p> : <SignIn />}</div>;
+    return (
+        <div id="authentication">
+            {isAuth ? <UserSettings /> : <SignIn />}
+        </div>
+    );
 }
-
 export default LogIn;
